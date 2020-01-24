e5f08d496c61fb4c4a5b7a4d0b4c0deb61f550a7
Jorge Medina Sistemas <jorge.si.med@gmail.com>
2020-01-24 05:17:53 -0600

    Agregando el metodo isLoogedIn y isNotLoggedIn para validar si esta autenticado

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,14 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../database'); // hace referencia a la conexion a la base de datos
-
-router.get('/add', (req, res) => {
+const {
+    isLoggedIn
+} = require('../lib/auth'); // Validar que este autenticado
+router.get('/add', isLoggedIn, (req, res) => {
     res.render('links/add');
 })
 
 // add
-router.post('/add', async(req, res) => {
-    const { title, url, description } = req.body; //Estamos utilizando destructuring adquiriendo propiedades especificas
+router.post('/add', isLoggedIn, async(req, res) => {
+    const {
+        title,
+        url,
+        description
+    } = req.body; //Estamos utilizando destructuring adquiriendo propiedades especificas
     const newLink = {
         title,
         url,
@@ -24,34 +30,47 @@ router.post('/add', async(req, res) => {
 
 
 // list
-
-router.get('/', async(req, res) => {
+router.get('/', isLoggedIn, async(req, res) => {
     const links = await pool.query('SELECT * FROM links');
     //console.log(links);
-    res.render('links/list', { links }); // a estas rutas les precede en la url /links/
+    res.render('links/list', {
+        links
+    }); // a estas rutas les precede en la url /links/
 
 });
 
-router.get('/delete/:id', async(req, res) => {
+router.get('/delete/:id', isLoggedIn, async(req, res) => {
 
-    const { id } = req.params;
+    const {
+        id
+    } = req.params;
     await pool.query(' DELETE FROM links WHERE id = ?', [id]);
     req.flash('success', 'Link Removed successfully')
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async(req, res) => {
+router.get('/edit/:id', isLoggedIn, async(req, res) => {
 
-    const { id } = req.params;
+    const {
+        id
+    } = req.params;
     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
     console.log(links);
 
-    res.render('links/edit', { link: links[0] });
+    res.render('links/edit', {
+        link: links[0]
+    });
 });
 
-router.post('/edit/:id', async(req, res) => {
-    const { id } = req.params;
-    const { title, url, description } = req.body; //Estamos utilizando destructuring adquiriendo propiedades especificas
+router.post('/edit/:id', isLoggedIn, async(req, res) => {
+    const {
+        id
+    } = req.params;
+    const {
+        title,
+        url,
+        description
+    } = req.body; //Estamos utilizando destructuring adquiriendo propiedades especificas
     const linkEdit = {
         title,
         url,
