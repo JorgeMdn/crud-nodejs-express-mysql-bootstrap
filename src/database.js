5e20db4fe960ae6138232dc1c3e32f59e50f8c0d
Jorge Medina Sistemas <jorge.si.med@gmail.com>
2020-01-24 01:01:19 -0600

    Connection to database mysql

---
+++ src/database.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+// el modulo mysql no acepta promesas asi que utilizamos promisify para convertir callbacks a promesas 
+const { promisify } = require('util');
+const { database } = require('./keys');
+
+// createPool() = tiene una especie de hilos que se van ejecutando y cada uno lo hace una a la vez
+const pool = mysql.createPool(database); // coneccion a la db
+
+// esto se hace para no estarse conectado cada vez que se utiliza
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('DATABASE CONNECTION WAS CLOSED');
+        }
+        if (err.code === 'ERR_CON_COUNT_ERROR') {
+            console.error('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+
+    if (connection) {
+        connection.release();
+        console.log('DB is Connected');
+        return;
+    }
+
+});
+
+// Promisify pool querys
+pool.query = promisify(pool.query)
+
+module.exports = pool;
